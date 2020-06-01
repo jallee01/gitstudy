0ccea1f607b60a4f3f286ddb5d7f433c0fd00a9d
jallee01 <jallee2301@gmail.com>
2020-06-01 15:26:46 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -4,10 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
+		helloName("Kakao");
+		helloName("Elsa");
+		
 
 	}
 	public static void hello() {
 		System.out.println("Hello");
+	}
+	
+	public static void helloName(String name) {
+		System.out.println("Hello" + name);
 	}
 
 }
